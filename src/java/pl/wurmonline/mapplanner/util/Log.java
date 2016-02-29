f69dfd0fd9002515f78684a133e9a581d9ae0bcd
Warlander <maciej@Warlander>
2016-02-29 17:01:19 +0100

    Logging partially implemented.

--- src/java/pl/wurmonline/mapplanner/util/Log.java
+++ src/java/pl/wurmonline/mapplanner/util/Log.java
@@ -1,17 +1,31 @@
 package pl.wurmonline.mapplanner.util;
 
+import java.time.Instant;
+
 public class Log {
     
     public static void info(Object object, String message) {
-        
+        info(object, object.getClass(), message);
     }
     
     public static void info(Class clazz, String message) {
-        
+        info(null, clazz, message);
     }
     
     private static void info(Object object, Class clazz, String message) {
+        StringBuilder build = new StringBuilder();
+        build.append("[").append(Instant.now().toString()).append("] ");
+        build.append("{").append(clazz.getSimpleName()).append("} ");
+        if (object != null) {
+            build.append("(").append(object.toString()).append(") ");
+        }
+        build.append(message);
         
+        System.out.println(build.toString());
+    }
+    
+    public static void error(Exception ex) {
+        ex.printStackTrace(System.err);
     }
     
 }

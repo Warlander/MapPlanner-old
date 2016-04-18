5a5413063e4c27d12234cc1b5f52fe3b200d08ee
Warlander <maciej@Warlander>
2016-04-18 14:26:02 +0200

    Changed version number.

--- src/java/pl/wurmonline/mapplanner/Constants.java
+++ src/java/pl/wurmonline/mapplanner/Constants.java
@@ -2,8 +2,8 @@ package pl.wurmonline.mapplanner;
 
 public class Constants {
     
-    public static final String VERSION_STRING = "MapPlanner PRE-ALPHA 7";
-    public static final int VERSION_NUMBER = 0_001_007;
+    public static final String VERSION_STRING = "MapPlanner PRE-ALPHA 8";
+    public static final int VERSION_NUMBER = 1001008;
     
     public static final boolean DEBUG = true;
     
